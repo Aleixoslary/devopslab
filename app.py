6e05a4bd34e5e6a99a3ffd065326394ab78050e7
Larissa Aleixos <larissa.aleixos@simbiox.com.br>
2022-10-05 22:48:12 -0300

    Fix CSRF

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)                                                                                                                           
+
 @app.route("/")
 def pagina_inicial():
-    return "Hello World Lari"
+    return "Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
     app.run()
