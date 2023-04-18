945217cf04fcd376d36130e6a7beff28fbe83f44
Larissa Aleixos <54966266+Aleixoslary@users.noreply.github.com>
2023-04-18 20:39:47 -0300

    fix test

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Teste Lari")
+        self.assertEqual(self.result.data.decode('utf-8'), "Teste 2")
